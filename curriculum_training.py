d5e42ae246852bcaf19c8210ec0a8927469d7c2d
Ridwan Salahuddeen <ridzy619@yahoo.com>
2025-04-13 18:02:27 +0000

    Improve experiment naming

--- curriculum_training.py
+++ curriculum_training.py
@@ -630,10 +630,11 @@ def main(cfg: DictConfig):
         )
         wandb_experiment = (
             f"{start_experiment + experiment:02} cube "
-            + str(subvolume_shape[0])
-            + " "
-            + collections[experiment]
-            + model_label
+            f"{subvolume_shape[0]} "
+            f"{collections[experiment]}"
+            f"-mx-it-{max_iters[experiment]}"
+            f"-it-ev-{cfg.experiment.iter_every_n_layers}"
+            f"-alpha-{cfg.experiment.alpha}"
         )
 
         # Set database parameters
